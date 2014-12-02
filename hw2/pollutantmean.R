afddad343dd86b3e64e2b66d3622a5084cf30970
Zhonghua Xi <xizhonghua@gmail.com>
2014-12-02 15:16:25 -0500

    Update pollutantmean.R

--- hw2/pollutantmean.R
+++ hw2/pollutantmean.R
@@ -1,3 +1,14 @@
+## Write a function named 'pollutantmean' that calculates the
+## mean of a pollutant (sulfate or nitrate) across a specified
+## list of monitors. The function 'pollutantmean' takes three
+## arguments: 'directory', 'pollutant', and 'id'. Given a 
+## vector monitor ID numbers, 'pollutantmean' reads that 
+## monitors' particulate matter data from the directory 
+## specified in the 'directory' argument and returns the mean 
+## of the pollutant across all of the monitors, ignoring any 
+## missing values coded as NA. A prototype of the function is
+## as follows
+
 pollutantmean <- function(directory, pollutant, id = 1:332) {
     ## 'directory' is a character vector of length 1 indicating
     ## the location of the CSV files
@@ -28,6 +39,7 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
         table <- c(table, data)
     }
     
-    ## remove the NAs, and compute the mean on all rows acrossing all files
+    ## remove the NAs, and compute the mean on all rows acrossing
+    ## all files
     mean(table, na.rm = T)
 }
