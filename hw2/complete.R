bbeaaa85e2a8c48219b8585dc1dc290ca4c1c4c9
Zhonghua Xi <xizhonghua@gmail.com>
2014-12-02 14:59:52 -0500

    add hw2

---
+++ hw2/complete.R
@@ -0,0 +1,30 @@
+complete <- function(directory, id = 1:332) {
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files
+    
+    ## 'id' is an integer vector indicating the monitor ID numbers
+    ## to be used
+    
+    ## Return a data frame of the form:
+    ## id nobs
+    ## 1  117
+    ## 2  1041
+    ## ...
+    ## where 'id' is the monitor ID number and 'nobs' is the
+    ## number of complete cases
+    
+    ids = numeric();
+    nobs = numeric();
+    
+    for (i in id) {
+        filename <- sprintf("%s/%03d.csv",directory,i)
+        data <- read.csv(filename)
+        nob <- nrow(data[complete.cases(data), ])
+        ids = c(ids, i)
+        nobs = c(nobs, nob)
+    }
+    
+    df <- data.frame(ids, nobs)
+    
+    df
+}
