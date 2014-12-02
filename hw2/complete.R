75022e716da997ced5887c6e987b0f0ec1c1c25e
Zhonghua Xi <xizhonghua@gmail.com>
2014-12-02 15:26:14 -0500

    Update complete.R

--- hw2/complete.R
+++ hw2/complete.R
@@ -13,18 +13,27 @@ complete <- function(directory, id = 1:332) {
     ## where 'id' is the monitor ID number and 'nobs' is the
     ## number of complete cases
     
+    ## create empty vectors
     ids = numeric();
     nobs = numeric();
     
+    ## loop all ids
     for (i in id) {
+        ## generate the filename
         filename <- sprintf("%s/%03d.csv",directory,i)
+        ## read the csv file
         data <- read.csv(filename)
+        ## 1. use complete.cases() to test complete cases
+        ## 2. index the complete cases
+        ## 3. count the complete cases via rnow()
         nob <- nrow(data[complete.cases(data), ])
+        ## append the id and number of complete cases to the vector
         ids = c(ids, i)
         nobs = c(nobs, nob)
     }
     
+    ## construct the output data frame
     df <- data.frame(ids, nobs)
-    
+    ## output the data frame
     df
 }
