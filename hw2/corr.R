cae63a984cdf36874bb02e247ad882140443da9c
Zhonghua Xi <xizhonghua@gmail.com>
2014-12-02 15:29:23 -0500

    Update corr.R

--- hw2/corr.R
+++ hw2/corr.R
@@ -9,17 +9,28 @@ corr <- function(directory, threshold = 0) {
     
     ## Return a numeric vector of correlations
     
+    ## create an empty vector
     CO <- numeric()
     
+    ## loop all ids (hard coded here...)
     for (i in 1:332) {
+        ## generate the filename
         filename <- sprintf("%s/%03d.csv",directory,i)
+        ## read the data
         data <- read.csv(filename)
+        ## subset the complete cases
         data <- data[complete.cases(data), ]
+        ## count the number of complete cases
         nob <- nrow(data)
+        ## compare with the threshold
         if (nob > threshold) {
+            ## compute the corrlation of two columns
             co <- cor(data[,2], data[,3])
+            ## append the result
             CO <- c(CO, co)    
         }
     }
+    
+    ## output the result
     CO
 }
